1442cc5e4da30baf504580fc77e4127541f0fe6e
0rahma0 <03rahma.ahmed@gmail.com>
2025-11-30 20:56:42 +0200

    menus

---
+++ Assets/Scripts/main_menu_manager_script.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class main_menu_manager_script : MonoBehaviour
+{
+    public Canvas loadingScreen;
+    private Canvas menu;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        loadingScreen.enabled = false;
+        menu = GetComponent<Canvas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void start_game()
+    {
+        StartCoroutine(loadStart());
+    }
+
+    IEnumerator loadStart()
+    {
+        loadingScreen.enabled=true;
+        menu.enabled = false;
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("village");
+        loadingScreen.enabled=false;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
